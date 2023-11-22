89c2a29291460674ebc1482ca24f59ec96e9e82d
Burty11 <142454463+Burty11@users.noreply.github.com>
2023-11-22 19:22:33 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+import random
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    return data
+def generate_random_data():
+def main():
